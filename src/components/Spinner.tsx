0e1be7c1328255d554709e7cadac0c02303e3498
dawidseipold <seipold.dawid@gmail.com>
2024-11-29 01:47:29 +0100

    chore: edit styles for the main component and spinner component

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,6 +1,6 @@
 export const Spinner = () => {
   return (
-    <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+    <svg className="spinner absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
       <circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
     </svg>
   );
